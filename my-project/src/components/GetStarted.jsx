2cd78bc503248edb0d10b08e66d85f35e121a63a
Tunybrainy <you@example.com>
2023-05-19 14:59:17 +0100

    hero_stage

--- my-project/src/components/GetStarted.jsx
+++ my-project/src/components/GetStarted.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import styles from "../style" ;
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+
+const GetStarted = () => (
+    <div className= {` ${styles.flexCenter} 
+    h-[120px] w-[120px] bg-blue-gradient
+    rounded-full p-[2px] cursor-pointer`}>
+      <div className={` ${styles.flexCenter} flex-col
+       w-[100%] h-[100%] rounded-full bg-primary `}>
+        <div className={` ${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium
+          text-[18px] leading-[23px] mr-2 ">
+            <span className=" text-gradient">Get</span>
+          </p>
+          <img src={arrowUp} alt="arrow"
+          className=" object-contain h-[23px] w-[23px]
+          " />
+        </div>
+        <p className="font-poppins font-medium
+          text-[18px] leading-[23px] ">
+            <span className=" text-gradient">Started</span>
+          </p>
+
+      </div>
+
+    </div>
+)
+
 
 export default GetStarted
